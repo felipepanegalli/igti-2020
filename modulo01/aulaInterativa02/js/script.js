30ef0ced46b90123a7887e8f82ad523a88f8f7ed
Felipe <felipe.panegalli@gmail.com>
2020-06-08 11:42:10 -0300

    Aula Interativa 02 - Module 01 - Done

--- modulo01/aulaInterativa02/js/script.js
+++ modulo01/aulaInterativa02/js/script.js
@@ -1,4 +1,5 @@
-const userApi = 'https://randomuser.me/api/?results=100&seed=promisse&nat=us,fr,au,br';
+const userApi =
+  'https://randomuser.me/api/?results=100&seed=promisse&nat=us,fr,au,br';
 const countryApi = 'https://restcountries.eu/rest/v2/all';
 
 let globalCountries = [];
@@ -6,72 +7,78 @@ let globalUsers = [];
 let globalUsersCountries = [];
 
 let divUsers = null;
+let divLoader = null;
 
+window.addEventListener('load', start);
 
-window.addEventListener('load', () => {
-    fetchUsers();
-    fetchCountries();
-    joinUsersAndCountries();
-    render();
-});
+async function start() {
+  divUsers = document.querySelector('#divUsers');
+  divLoader = document.querySelector('.loader-container');
+  await fetchUsers();
+  await fetchCountries();
+  joinUsersAndCountries();
+  render();
+}
 
 const fetchUsers = async () => {
-    const res = await fetch(userApi);
-    const json = await res.json()
-    globalUsers = json.results.map(({picture, name, nat}) => {
-        return {
-            name: name.first,
-            country: nat,
-            picture: picture.large,
-        };
-    });
-}
+  const res = await fetch(userApi);
+  const json = await res.json();
+  globalUsers = json.results.map(({ picture, name, nat }) => {
+    return {
+      name: name.first,
+      country: nat,
+      picture: picture.large,
+    };
+  });
+};
 
 const fetchCountries = async () => {
-    const res = await fetch(countryApi);
-    const json = await res.json();
-    globalCountries = json.map(({alpha2Code, flag}) => {
-        return {
-            code: alpha2Code,
-            flag: flag,
-        };
-    });
-}
+  const res = await fetch(countryApi);
+  const json = await res.json();
+  globalCountries = json.map(({ alpha2Code, flag }) => {
+    return {
+      code: alpha2Code,
+      flag: flag,
+    };
+  });
+};
 
 const joinUsersAndCountries = () => {
-    globalUsers.forEach((user) => {
-        const userCountry = globalCountries.find((country) => {
-            return country.code === user.country;
-        });
-        if (userCountry) {
-            globalUsersCountries.push({...user, ...userCountry});
-        }
+  globalUsers.forEach((user) => {
+    const userCountry = globalCountries.find((country) => {
+      return country.code === user.country;
     });
+    if (userCountry) {
+      globalUsersCountries.push({ ...user, ...userCountry });
+    }
+  });
 };
 
 const renderUserInfo = () => {
-    divUsers = document.querySelector('#divUsers');
-    let usersHtml = '<div class="col-sm-12 col-md-3 col-lg-4">';
-    globalUsersCountries.forEach((user) => {
-        let userHtml = `
+  let usersHtml = '<div class="row">';
+  globalUsersCountries.forEach((user) => {
+    let userHtml = `
+    <div class="col-sm-12 col-md-4 col-lg-3">
       <div class="user">
         <div class="picture">
-          <img src="${user.picture}">
+          <img src="${user.picture}" class="rounded-circle">
         </div>
         <div class="data">
           <span>${user.name}</span>
           <img src="${user.flag}">
         </div>
       </div>
+    </div>
     `;
-        usersHtml += userHtml;
-    });
+    usersHtml += userHtml;
+    divLoader.classList.add('d-none');
+    divUsers.classList.remove('d-none');
+  });
 
-    usersHtml += '</div>';
-    tabUsers.innerHTML = usersHtml;
-    console.log(usersHtml)
+  usersHtml += '</div>';
+  divUsers.innerHTML = usersHtml;
 };
 
 const render = () => {
-    renderUserInfo();
+  renderUserInfo();
 };
