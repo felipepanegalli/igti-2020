fbdf5258174a8254c2ed08692beeaf3de64bde14
Felipe <felipe.panegalli@gmail.com>
2020-06-22 11:34:26 -0300

    Trabalho Pratico Mod 03 Done

--- modulo03/trabalhoPratico/react-salario/src/App.js
+++ modulo03/trabalhoPratico/react-salario/src/App.js
@@ -3,6 +3,7 @@ import Bar from './components/Bar';
 import InputText from './components/InputText';
 import { calculateSalaryFrom } from './helpers/salary';
 import { formatNumber, currencyFormat } from './helpers/formatHelpers';
+import Chart from 'react-apexcharts';
 
 export default class App extends Component {
   constructor() {
@@ -20,6 +21,20 @@ export default class App extends Component {
       porc1: 0,
       porc2: 0,
       porc3: 0,
+      chartType: 'donut',
+      chartOptions: {
+        labels: ['Desconto INSS', 'Desconto IRPF', 'Salário Líquido'],
+        colors: ['#e67e22', '#c0392b', '#16a085'],
+        legend: { position: 'top' },
+        tooltip: {
+          y: {
+            formatter: (value) => {
+              return formatNumber(value) + '%';
+            },
+          },
+        },
+      },
+      chartSeries: [0, 0, 0],
     };
   }
 
@@ -53,12 +68,16 @@ export default class App extends Component {
       porc1: b1,
       porc2: b2,
       porc3: b3,
+      chartSeries: [b1, b2, b3],
     });
     if (salary === '') {
       this.setState({
         bar1: 0,
         bar2: 0,
         bar3: 100,
+        porc1: 0,
+        porc2: 0,
+        porc3: 0,
         inssBase: 0,
       });
     }
@@ -90,79 +109,104 @@ export default class App extends Component {
       porc3,
     } = this.state;
 
-    const { joinBars, orangeColor, redColor, greenColor, bold } = styles;
+    const {
+      joinBars,
+      orangeColor,
+      redColor,
+      greenColor,
+      bold,
+      chartCenter,
+      border,
+    } = styles;
 
     return (
-      <div className="container">
-        <div>
-          <h3 className="center">React Salário</h3>
-        </div>
-
-        <div className="row">
-          <div className="col s12">
-            <InputText
-              labelFor="salary"
-              label="Salário bruto:"
-              editable={false}
-              value={salary}
-              onChange={this.handleSalaryInputHandle}
-            />
+      <div style={border}>
+        <div className="container">
+          <div>
+            <h3 className="center">React Salário</h3>
           </div>
 
-          <div className="col s12 m3">
-            <InputText
-              labelFor="inssBase"
-              label="Base INSS:"
-              editable={true}
-              value={currencyFormat(inssBase)}
-              style={bold}
-            />
-          </div>
+          <div className="row">
+            <div className="col s12">
+              <InputText
+                labelFor="salary"
+                label="Salário bruto:"
+                editable={false}
+                value={salary}
+                onChange={this.handleSalaryInputHandle}
+              />
+            </div>
 
-          <div className="col s12 m3">
-            <InputText
-              labelFor="inssDesc"
-              label="Desconto INSS:"
-              editable={true}
-              value={currencyFormat(inssDesc) + ` (${formatNumber(porc1)}%)`}
-              style={{ ...orangeColor, ...bold }}
-            />
-          </div>
+            <div className="col s12 m3">
+              <InputText
+                labelFor="inssBase"
+                label="Base INSS:"
+                editable={true}
+                value={currencyFormat(inssBase)}
+                style={bold}
+              />
+            </div>
 
-          <div className="col s12 m3">
-            <InputText
-              labelFor="irpfBase"
-              label="Base IRPF:"
-              editable={true}
-              value={currencyFormat(irpfBase)}
-              style={bold}
-            />
-          </div>
+            <div className="col s12 m3">
+              <InputText
+                labelFor="inssDesc"
+                label="Desconto INSS:"
+                editable={true}
+                value={currencyFormat(inssDesc) + ` (${formatNumber(porc1)}%)`}
+                style={{ ...orangeColor, ...bold }}
+              />
+            </div>
 
-          <div className="col s12 m3">
-            <InputText
-              labelFor="irpfDesc"
-              label="Desconto IRPF:"
-              editable={true}
-              value={currencyFormat(irpfDesc) + ` (${formatNumber(porc2)}%)`}
-              style={{ ...redColor, ...bold }}
-            />
-          </div>
+            <div className="col s12 m3">
+              <InputText
+                labelFor="irpfBase"
+                label="Base IRPF:"
+                editable={true}
+                value={currencyFormat(irpfBase)}
+                style={bold}
+              />
+            </div>
 
-          <div className="col s12">
-            <InputText
-              labelFor="salaryFull"
-              label="Salário Líquido:"
-              editable={true}
-              value={currencyFormat(salaryFull) + ` (${formatNumber(porc3)}%)`}
-              style={{ ...greenColor, ...bold }}
-            />
-          </div>
-          <div className="col s12">
-            <div style={joinBars}>
-              <Bar value={bar1} color={'#e67e22'} />
-              <Bar value={bar2} color={'#c0392b'} />
-              <Bar value={bar3} color={'#16a085'} />
+            <div className="col s12 m3">
+              <InputText
+                labelFor="irpfDesc"
+                label="Desconto IRPF:"
+                editable={true}
+                value={currencyFormat(irpfDesc) + ` (${formatNumber(porc2)}%)`}
+                style={{ ...redColor, ...bold }}
+              />
+            </div>
+
+            <div className="col s12">
+              <InputText
+                labelFor="salaryFull"
+                label="Salário Líquido:"
+                editable={true}
+                value={
+                  currencyFormat(salaryFull) + ` (${formatNumber(porc3)}%)`
+                }
+                style={{ ...greenColor, ...bold }}
+              />
+            </div>
+            <div className="col s12">
+              <div style={joinBars}>
+                <Bar value={bar1} color={'#e67e22'} />
+                <Bar value={bar2} color={'#c0392b'} />
+                <Bar value={bar3} color={'#16a085'} />
+              </div>
+            </div>
+            <div className="col s12" style={{ marginTop: '40px' }}>
+              <div className="donut" style={chartCenter}>
+                <Chart
+                  options={this.state.chartOptions}
+                  series={this.state.chartSeries}
+                  type={this.state.chartType}
+                  width="380"
+                />
+              </div>
+            </div>
+            <div className="col s12 teal-text center-align" style={bold}>
+              <p className="bold">Desenvolvido por Felipe Panegalli - IGTI</p>
             </div>
           </div>
         </div>
@@ -189,5 +233,15 @@ const styles = {
   },
   bold: {
     fontWeight: 'bold',
+  },
+  chartCenter: {
+    textAlign: '-webkit-center',
+  },
+  border: {
+    borderRadius: '5px',
+    margin: '40px 50px',
+    padding: '20px',
+    boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.5)',
+    background: 'rgba(255, 255, 255, .9)',
   },
 };
