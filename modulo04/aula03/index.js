06210cbed51465cc8c86cae2bad3c7cd27588a40
Felipe <felipe.panegalli@gmail.com>
2020-07-08 13:15:15 -0300

    Class 03 - Module 04 Done

---
+++ modulo04/aula03/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb://${process.env.USERDB}:${process.env.PASSDB}localhost:27017/local`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log('Conectado com sucesso');
+  } catch (error) {
+    console.log('Erro ao conectar ao banco:\n' + error);
+  }
+})();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log('Servidor em execução');
+});
